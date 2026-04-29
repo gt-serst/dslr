d6e4c926eacd9bebe0133a536238586a6418869f
Géraud t'Serstevens <geraudts@gmail.com>
2026-04-29 22:53:34 +0200

    label encoding + standardize numericals

--- logreg_train.py
+++ logreg_train.py
@@ -1,22 +1,33 @@
 import sys
 import pandas as pd
+import numpy as np
 
 if __name__ == '__main__':
 
 	try:
 		if len(sys.argv) == 2:
 			df = pd.read_csv(sys.argv[1])
-			print(df)
+			df.columns = df.columns.str.replace(" ", "_").str.lower()
+			# Drop not meaningful variables
+			df = df.drop(["index", "first_name", "last_name", "birthday"], axis=1)
+			# Isolate dependant variable
+			y = df["hogwarts_house"]
+			X = df.drop("hogwarts_house", axis=1)
+			num_features = X.select_dtypes(include=[int, float]).columns
+			cat_features = X.select_dtypes(include=str).columns
+			# Handle NA values
 			cols = df.isna().any()
 			cols_with_na = cols[cols].index
-			print(cols_with_na)
 			for col in cols_with_na:
-				df[col] = df[col].fillna(df[col].mean())
-			print(df.select_dtypes(include=[str]).columns)
-			print(df.dtypes)
-			df = df.drop(["First Name", "Last Name", "Birthday"], axis=1)
-			df = pd.get_dummies(df, dtype=int)
-			print(df)
+				X[col] = X[col].fillna(X[col].mean())
+			# Perform Label Encoding
+			X.loc[X["best_hand"] == "Left", "best_hand"] = str(1)
+			X.loc[X["best_hand"] == "Right", "best_hand"] = str(0)
+			X["best_hand"] = X["best_hand"].astype(int)
+			# Standardize numerical features
+			for col in num_features:
+				X[col] = (X[col] - X[col].mean()) / X[col].std()
+			print(X)
 		else:
 			raise BaseException("program must take one argument")
 	except FileNotFoundError as e:
