80efb5f71cc5d9526febeb566514f3307ec7c869
Géraud t'Serstevens <geraudts@gmail.com>
2026-08-14 13:29:04 +0200

    adapting training function for new optimization algorithms

--- model.py
+++ model.py
@@ -8,30 +8,40 @@ def compute_error(X, y_binary, y_pred):
 	cost = -1/m * np.sum(y_binary * np.log(y_pred) + (1 - y_binary) * np.log(1 - y_pred))
 	return cost
 
-def calculate_gradient(y_pred, y_binary, X):
-	m = len(X)
-	return (1/m) * X.T.dot(y_pred - y_binary)
+def calculate_gradient(y_pred, y_batch, X_batch):
+	m = len(X_batch)
+	return (1/m) * X_batch.T.dot(y_pred - y_batch)
 
 def gradient_descent(X, y_binary, alpha, epochs, batch_size, thetas_array):
+	m = len(X)
 	epsilon = 1e-6
 	previous_cost = None
 	cost_history = []
 	for epoch in range(epochs):
-		# Predict with sigmoid function
+		# Shuffle the dataset
+		indices = np.random.permutation(m)
+		X_shuffled = X[indices]
+		y_shuffled = y_binary[indices]
+		# Mini-batches
+		for i in range(0, m, batch_size):
+			X_batch = X_shuffled[i:i + batch_size]
+			y_batch = y_shuffled[i:i + batch_size]
+			# Prediction for this batch
+			y_pred_batch = predict_belongs_to_house(X_batch, thetas_array)
+			# Gradient for this batch
+			gradient_thetas = calculate_gradient(y_pred_batch, y_batch, X_batch)
+			# Update theta
+			thetas_array = update_thetas(alpha, thetas_array, gradient_thetas)
+		# Evaluate the model on the complete dataset
 		y_pred = predict_belongs_to_house(X, thetas_array)
-		# Compute error between prediction and target vector with logloss function
 		cost = compute_error(X, y_binary, y_pred)
-		if epochs % 100 == 0:
-			print(f"Epoch {epoch}, Cost: {cost}")
 		cost_history.append(cost)
+		if epoch % 100 == 0:
+			print(f"Epoch {epoch}, Cost: {cost}")
 		if previous_cost is not None:
-			# Stop if the cost is no longer decreasing significantly
 			if abs(previous_cost - cost) < epsilon:
 				break
 		previous_cost = cost
-		gradient_thetas = calculate_gradient(y_pred, y_binary, X)
-		# If not need to update thetas (move a little to find if the next position could be the minimun)
-		thetas_array = update_thetas(alpha, thetas_array, gradient_thetas)
 	return thetas_array
 
 def update_thetas(alpha, thetas_array, gradient_thetas):
